a59140d3a17919ff2990f1bc461d28a48ef51b5a
Agnaldo Cardoso Junior <acjunior@Agnaldos-MBP.lan>
2022-06-02 22:38:39 +0100

    add all required graphs for index

--- assets/js/index.js
+++ assets/js/index.js
@@ -42,12 +42,68 @@ const renderKpis = (data) => {
   renderDate();
 };
 
-const renderPieChart = () => { };
+const renderPieChart = (data) => {
+  const ctx = document.getElementById('pie-chart');
+  const chartConfig = {
+    labels: ['Confirmados', 'Recuperados', 'Mortes'],
+    datasets: [{
+      label: 'Covid Global',
+      data: [data.NewConfirmed, data.NewRecovered, data.NewDeaths],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  }
+  new Chart(ctx, {
+    type: 'pie',
+    data: chartConfig
+  });
+};
+
+const renderBarChart = (data) => {
+  const sortByProp = (prop) => (arr) => arr.sort((a, b) => b[prop] - a[prop]);
+  const takeTen = (arr) => arr.slice(0, 10);
+  const TotalDeaths = (c) => c.TotalDeaths;
+  const CountryName = (c) => c.Country;
+
+  const top10Countries = _.compose(takeTen, sortByProp('TotalDeaths'))(data);
+  const chartData = top10Countries.map(TotalDeaths);
+  const chartLabels = top10Countries.map(CountryName);
+
+  const chartConfig = {
+    labels: chartLabels,
+    datasets: [{
+      label: 'Top 10 Covid Global',
+      data: chartData,
+      backgroundColor: [
+        'rgba(153, 102, 255)'
+      ],
+      hoverOffset: 4
+    }]
+  }
+
+  const ctx = document.getElementById('bar');
+  new Chart(ctx, {
+    type: 'bar',
+    data: chartConfig,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+};
 
 // Main
 (async () => {
   const covidStatus = await getCovidStatusSummary();
+  console.log(covidStatus)
   renderKpis(covidStatus.Global);
-  renderPieChart();
-  renderBarChart();
+  renderPieChart(covidStatus.Global);
+  renderBarChart(covidStatus.Countries);
 })();
